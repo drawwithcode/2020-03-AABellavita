6d7868b6e3c728191faf486e32835097a582192e
AABellavita <andrea.bellavita@mail.polimi.it>
2020-10-18 22:15:58 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,8 +32,8 @@ function setup() {
     analyzer.setInput(routeSong);
 
     imageMode(CENTER);
-    image(myLoad, width/2, height/2-180, 330, 215);
-    image(myGameboy, width/2, height/2, 375, 666);
+    image(myLoad, width / 2, height / 2 - 180, 330, 215);
+    image(myGameboy, width / 2, height / 2, 375, 666);
 
     // Button Play
     push();
@@ -77,8 +77,8 @@ function draw() {
         image(myGirl, 0, 0, width / 2, height);
         image(myGirl2, width / 2, 0, width / 2, height);
         imageMode(CENTER);
-        image(myTrump, width / 2 - 200 - volume, 0 + 350, height / 5, volume * 5);
-        image(myTrump2, width / 2 + 200 + volume, 0 + 350, height / 5, volume * 5);
+        image(myTrump, width / 2 - 250 - volume, height / 2, height / 5, volume * 5);
+        image(myTrump2, width / 2 + 250 + volume, height / 2, height / 5, volume * 5);
         image(myGameboy, width / 2, height / 2, 375, 666);
         let myScreen = new RubyImage();
         myScreen.display();
@@ -191,8 +191,8 @@ function windowResized() {
         image(myGirl, 0, 0, width/2, height);
         image(myGirl2, width / 2, 0, width / 2, height);
         imageMode(CENTER)
-        image(myTrump, width / 2 - 200 - volume, 0 + 350, height/5, volume * 5);
-        image(myTrump2, width / 2 + 200 + volume, 0 + 350, height / 5, volume * 5);
+        image(myTrump, width / 2 - 250 - volume, height / 2, height / 5, volume * 5);
+        image(myTrump2, width / 2 + 250 + volume, height / 2, height / 5, volume * 5);
         let myScreen = new RubyImage();
         myScreen.display();
     }
